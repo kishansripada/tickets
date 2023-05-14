f13f654e9e2806dc2a78df5b734d5e95f9555def
kishansripada <kishansripada@gmail.com>
2023-05-14 18:53:10 -0400

    typescript ignore

--- next.config.js
+++ next.config.js
@@ -3,7 +3,20 @@ const nextConfig = {
     experimental: {
         appDir: true,
         serverActions: true,
-    }
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+
 }
 
 module.exports = nextConfig

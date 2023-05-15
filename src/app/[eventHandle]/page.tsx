2888af33cff38e044059ce8f8957ecede89a4781
kishansripada <kishansripada@gmail.com>
2023-05-15 01:52:36 -0400

    tickets button

--- src/app/[eventHandle]/page.tsx
+++ src/app/[eventHandle]/page.tsx
@@ -137,7 +137,19 @@ export default async function Home({ params: { eventHandle } }: { params: { even
                   </div>
 
                   <Link href={`/${eventHandle}/form`}>
-                     <button className="bg-indigo-700 rounded-md px-4 py-2 text-white">get tickets</button>
+                     <div className="text-black bg-white  px-4 py-2 rounded-full flex flex-row items-center">
+                        <p>get tickets</p>
+                        <svg
+                           xmlns="http://www.w3.org/2000/svg"
+                           fill="none"
+                           viewBox="0 0 24 24"
+                           strokeWidth={1.5}
+                           stroke="currentColor"
+                           className="w-5 h-5 ml-2"
+                        >
+                           <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                        </svg>
+                     </div>
                   </Link>
                </div>
             </div>
